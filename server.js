3e3b02a10734c6dcc6a13f20ae044ff0c495c3a0
PengChen11 <peng.chen11@outlook.com>
2021-07-16 14:14:26 -0700

    done

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-const destinations = [
-  {
+const destinations = {
+  1234: {
     _id: "1234",
     name: "The Needle",
     location: "Seattle, WA",
@@ -14,7 +14,7 @@ const destinations = [
       "https://media.cntraveler.com/photos/59de395b18d971147f4bcc8e/4:3/w_3036,h_2277,c_limit/Seattle-Space-Needle_GettyImages-180262211.jpg",
     description: "",
   },
-  {
+  3453: {
     _id: "3453",
     name: "The Eiffel Tower",
     location: "Paris, France",
@@ -22,12 +22,17 @@ const destinations = [
       "https://lh5.googleusercontent.com/p/AF1QipMyFp7DDFxwIv3Idp4YKibGJ2zhm0l9UAsFIDDh=w203-h135-k-no",
     description: "Romantic place",
   },
-];
+};
 
 // CRUD
 // CREATE => POST
 server.post("/destinations", (req, res) => {
-  destinations.push(req.body);
+  const _id = idGenerator();
+  const newRecord = {
+    _id,
+    ...req.body
+  }
+  destinations[_id]=newRecord;
 
   res.send({ status: "success" });
 });
@@ -40,31 +45,27 @@ server.get("/destinations", (req, res) => {
 // UPDATE => PUT
 server.put("/destinations", (req, res) => {
   const _id = req.query._id;
-
-  for (let i in destinations){
-    if (destinations[i]._id === _id){
-      destinations[i] = req.body;
-      console.log(req.body);
-      console.log(destinations[i])
-      res.send(`updated location record ${_id}`);
-      return;
-    }
+  const target = destinations[_id];
+  if (!target){
+    res.send(`can NOT find record ${_id}`);
+  } else {
+    destinations[_id] = {...req.body, _id};
+    res.send(`updated record id ${_id}`);
   }
-  res.send(`can NOT find record ${_id}`);
-  
 });
 
 // DELETE => DELETE
 server.delete("/destinations", (req, res) => {
   const { _id } = req.query;
-  for (let i in destinations){
-    if (destinations[i]._id === _id){
-      destinations.splice(i, 1);
-      res.send(`deleted location record ${_id}`);
-      return;
-    }
+  const target = destinations[_id];
+
+  if (!target){
+    res.send(`can NOT find record ${_id}`);
+  } else {
+    delete destinations[_id];
+    res.send(`deleted location record ${_id}`);
   }
-  res.send(`can NOT find record ${_id}`);
+  
 });
 
 const port = process.env.PORT || 3000;
@@ -72,3 +73,13 @@ const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log("Server listening");
 });
+
+
+function idGenerator(){
+  const newID = () => Math.floor(1000 + Math.random() * 9000);
+  let result = newID();
+  while (destinations[result]){
+    result = newID()
+  }
+  return result;
+}
